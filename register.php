bde75b0c7c5d5831f974f4d132dc0444d68b1563
Pin-Chyan Tsai <pitsai@c3r7s8.wethinkcode.co.za>
2019-10-14 00:45:44 +0200

    forms changed or public css

--- register.php
+++ register.php
@@ -1,77 +1,4 @@
 <link rel="stylesheet" href="styles/style.css">
-
-<style>
-
-* {
-  box-sizing: border-box;
-}
-
-/* Add padding to containers */
-.container {
-  padding: 16px;
-  background-color: white;
-}
-
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-  border: none;
-  background: white;
-}
-
-input[type=text]:focus, input[type=password]:focus {
-  background-color: #ddd;
-  outline: none;
-}
-
-/* Overwrite default styles of hr */
-hr {
-  border: 1px solid #f1f1f1;
-  margin-bottom: 25px;
-}
-
-/* Set a style for the submit button */
-.registerbtn {
-  background-color: blue;
-  color: white;
-  padding: 16px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-}
-
-.registerbtn:hover {
-  opacity: 1;
-}
-
-/* Add a blue text color to links */
-a {
-  color: dodgerblue;
-}
-
-/* Set a grey background color and center the text of the "sign in" section */
-.signin {
-  background-color:  white;
-  text-align: center;
-}
-
-
-
-    #register {
-        position: relative;
-        float: right;
-        margin-right: 150px;
-        opacity: 0.4;
-        width: 1000px;
-        height: 1238px;
-    }
-</style>
-
 <html>
     <head>
         <title>
@@ -121,7 +48,7 @@ a {
                     </div>
                 
                     <div class="container signin">
-                    <p>Already have an account? <a href="#">Sign in</a>.</p>
+                    <p>Already have an account? <a href="login">Log-In</a>.</p>
                 </div>
                 </form>
             </div>
