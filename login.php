d863ea05c22a92720b4d8105088641b0edb88621
Marthen van der Eems <marvan-d@c3r7s9.wethinkcode.co.za>
2019-10-13 23:20:03 +0200

    added sign in form

--- login.php
+++ login.php
@@ -1,4 +1,25 @@
 <link rel="stylesheet" href="styles/style.css">
+<style>
+    .login_title{
+        margin-top: 50px;
+        margin-bottom: 50px;
+        font-size: 75px;
+    }
+    input[type=text], input[type=password] {
+        border-radius: 14px;
+        padding: 30px;
+        width: 350px;
+        height: 10px;
+    }
+    .login_reg {
+        margin-top: 25px;
+        margin-bottom: 25px;
+        font-size: 40px;
+    }
+    .submit_button {
+
+    }
+</style>
 <html>
     <head>
         <title>
@@ -19,20 +40,28 @@
 
         <!-- Login form start -->
         <div class="log_in">
-        <h1 style="font-size: 50px">Sign in.</h1>
-        <form method="post" action="login.php">
-            <table>
-                <tr>
-                    <td><b>Username:</b></td>
-                    <td><input type="text" name="login" placeholder="enter username" required/></td>
-                </tr>
-                <tr>
-                    <td><b>Email:</b>
-                    <td><input type="password" name="pass" placeholder="enter password" required/></td>
-                </tr>
-            </table>
-          </form> 
-          <!-- Login form end -->
+            <div>
+                <h1 class="login_title">Sign in.</h1>
+            </div>
+            <div class="login_form">
+                <form method="post" action="login.php">
+                    <table>
+                        <tr>
+                            <td><input type="text" name="login" placeholder="Username" required/></td>
+                        </tr>
+                        <tr style="width: 350px; height: 10px;">
+                            <td><input type="password" name="pass" placeholder="Password" required/></td>
+                        </tr>
+                        <tr>
+                            <td colspan="8"><input type="submit" name="sign in" value="login" /></td>
+                        </tr>
+                    </table>
+                </form> 
+            </div>
+            <div class="login_reg">
+                <h1>No account? register here.</h1>
+            </div>
         </div>
+         <!-- Login form end -->
     </body>
 </html>
