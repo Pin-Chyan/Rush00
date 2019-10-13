f4c2893ff92943aa36ab9452b57ad197fc7208e4
Pin-Chyan Tsai <pitsai@c3r7s8.wethinkcode.co.za>
2019-10-13 20:23:32 +0200

    update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         </title>
     </head>
     <body>
-    <!-- nav bar -->
+    <!-- nav bar start -->
     <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#news">News</a></li>
@@ -19,16 +19,17 @@
             </div>
         </li>
     </ul>
+    <!-- nar bar end -->
 
     <!-- icons bar end -->
         <div class="h1_page">
         <ul>
-            <li><a href="#">Store</a></li>
+            <li id="h_button"><a href="#">Store</a></li>
         </ul>
         </div>
         <div class="h2_page">
         <ul>
-            <li><a href="login.php">Log-In</a></li>
+            <li id="h_button"><a href="login.php">Log-In</a></li>
         </ul>
         </div>
     <!-- icons bar end -->
