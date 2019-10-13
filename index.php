ec4e80399feb9fd8fe3561982f7c370a5e646da0
Pin-Chyan Tsai <pitsai@c3r7s8.wethinkcode.co.za>
2019-10-13 19:24:12 +0200

    front end

---
+++ index.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="style.css">
+<html>
+    <head>
+        <title>
+            Home Page
+        </title>
+    </head>
+    <body>
+        <div class="h1_page">
+            <a href="#"><div id="box">STORE</div></a>
+        </div>
+        <div class="h2_page">
+            <a href="login.php"><div id="box">LOG-IN</div></a>
+        </div>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="style.css">
+<html>
+    <head>
+        <title>
+            log-in
+        </title>
+    </head>
+    <body>
+        <div class="log_in">
+            <s
+        </div>
+    </body>
+</html>
