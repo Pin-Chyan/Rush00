ed4da05659c8bc45685c6a1765dd2f2bc61c01cc
Theunis Coetzee <tcoetzee@c3r9s3.wethinkcode.co.za>
2019-10-14 16:26:30 +0200

    all

--- store.php
+++ store.php
@@ -27,9 +27,40 @@ session_start();
             </div>
             <div class="s_center">
                 <h1 class="h_title">Category</h1>
+                <div class="navcat">
+                <a href="index.php?view=store&cat=Romantic">Romantic</a>
+                <a href="&cat=Comedy">Comedy</a>
+                <a href="&cat=Adventure">Adventure</a>
+                <a href="&cat=Action">Action</a>
+                <a href="&cat=Fantasy">Fantasy</a>
+                <a href="&cat=Isekai">Isekai</a>
+                <a href="&cat=Anime">Anime</a>
+                <a href="&cat=Manga">Manga</a>
+                <a href="#" style="margin-top: 250px;"><b>RESET FILTER</b></a>
+                </div>
             </div>
             <div class="s_right">
                     <h1 class="h_title">Products</h1>
+                    <?php
+                    if (!$cat && !$orig) {
+                        $products = db_get_products();
+                        foreach($products as $item) {
+                            include("item.php");
+                        }
+                    }
+                    else if ($cat) {
+                        $products = db_get_products_cat($cat);
+                        foreach($products as $item) {
+                            include("item.php");
+                        }
+                    }
+                    else if ($orig) {
+                        $products = db_get_products_orig($orig);
+                        foreach($products as $item) {
+                            include("item.php");
+                        }
+                    }
+                    ?>
               </div>    
             </div>        
         </div>
