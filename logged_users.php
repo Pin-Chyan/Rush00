6ff95cd50d2d858b74f3f95c6dcd01b87911d3cd
Pin-Chyan Tsai <pitsai@c3r7s8.wethinkcode.co.za>
2019-10-14 11:48:36 +0200

    dummy products in database -initdata

--- logged_users.php
+++ logged_users.php
@@ -1,8 +1,11 @@
 <link rel="stylesheet" href="styles/style.css">
+<?php
+    include('database/users.php');
+?>
 <html>
     <head>
         <title>
-            User Profile
+            User Profile1
         </title>
     </head>
     <body class="background">
@@ -11,7 +14,7 @@
             <!-- <li><a href="index.php"><img class="h_image" src="images/logo.png"></a></li> -->
             <li><a href="index.php">Home</a></li>
             <li><a href="store.php">Store</a></li>
-            <li><a href="login.php">Log-In</a></li>
+            <li><a href="login.php"><?php if (isset($_SESSION['name'])) {echo $_SESSION['name'];} else {echo "Sign-in";} ?></a></li>
             <li><a href="cart.php">Cart</a></li>
         </ul>
         <!-- nar bar end -->
@@ -23,6 +26,11 @@
             </div>
             <div class="l_center">
                 <h1 class="h_title">User Profile</h1>
+                <?php
+                    $db = mysqli_connect("localhost:3306","server01","memes","death");
+                    $current = mysqli_query($db, "SELECT username FROM users");
+                    echo "<h1 class='h_title'>This (get_val($current)) Bitch</h1>";
+                ?>
             </div>
         </div>
     </body>
