a7bd4a49c250ec363fbde1d29bba04be4c707cb4
Pin-Chyan Tsai <pitsai@c3r7s8.wethinkcode.co.za>
2019-10-14 11:17:52 +0200

    css conflict resolved

---
+++ logged_users.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="styles/style.css">
+<html>
+    <head>
+        <title>
+            User Profile
+        </title>
+    </head>
+    <body class="background">
+        <!-- nav bar start -->
+        <ul>
+            <!-- <li><a href="index.php"><img class="h_image" src="images/logo.png"></a></li> -->
+            <li><a href="index.php">Home</a></li>
+            <li><a href="store.php">Store</a></li>
+            <li><a href="login.php">Log-In</a></li>
+            <li><a href="cart.php">Cart</a></li>
+        </ul>
+        <!-- nar bar end -->
+
+        <!-- wrap start -->
+        <div class="l_wrapper">
+            <div class="l_left">
+            <img style="width: 1100px; height: 1000px;" src = "https://images2.alphacoders.com/246/246622.jpg">
+            </div>
+            <div class="l_center">
+                <h1 class="h_title">User Profile</h1>
+            </div>
+        </div>
+    </body>
+</html>
