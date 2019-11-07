9f2c880387ae5ac331524170d0fc640d350ab78f
Pin-Chyan Tsai <pitsai@c3r7s8.wethinkcode.co.za>
2019-11-07 09:27:59 +0200

    new branch

--- delete_user.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include('database/users.php');
-    session_start();
-    user_del($_SESSION['user_id']);
-    $_SESSION['user_id'] = NULL;
-    session_destroy();
-    header('location: index.php');
-?>
